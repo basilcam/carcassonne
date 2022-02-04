c0f86053aab4b99ae67afd7912fe2f82a1c8ea3d
Cam <basilcam@posteo.de>
2022-02-04 16:15:48 -0600

    add feature management test

--- src/test/java/net/basilcam/core/features/CompositeFeatureManagerTest.java
+++ src/test/java/net/basilcam/core/features/CompositeFeatureManagerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,9 +53,6 @@ class CompositeFeatureManagerTest {
 
         Collection<? extends Feature> features = this.featureManager.getFeatures();
         assertThat(features).hasSize(2);
-        assertThat(features.stream().map(Feature::getType).collect(Collectors.toList()))
-                .containsExactlyInAnyOrder(TileSectionType.CITY, TileSectionType.ROAD);
-
         assertThat(features).areExactly(1, new Condition<>(
                 feature -> feature.getType() == TileSectionType.CITY
                         && feature.isComplete(),
@@ -64,5 +60,96 @@ class CompositeFeatureManagerTest {
         assertThat(features).areExactly(1, new Condition<>(
                 feature -> feature.getType() == TileSectionType.ROAD && !feature.isComplete(),
                 "road"));
+    }
+
+    @Test
+    public void shouldUpdateFeatures_tilePlacedBesideTwoTiles() {
+        Tile tile15 = TileStackFactory.getTile(15);
+        Tile tile11 = TileStackFactory.getTile(11);
+        tile11.rotateClockwise();
+        tile11.rotateClockwise();
+        Tile tile20 = TileStackFactory.getTile(20);
+        tile20.rotateClockwise();
+        tile20.rotateClockwise();
+        tile20.rotateClockwise();
+
+        placeTileAndUpdate(tile15, 1, 0);
+
+        Collection<? extends Feature> features = this.featureManager.getFeatures();
+        assertThat(features).hasSize(2);
+        assertThat(features).areExactly(1, new Condition<>(
+                feature -> feature.getType() == TileSectionType.CITY
+                        && !feature.isComplete(),
+                "city"));
+        assertThat(features).areExactly(1, new Condition<>(
+                feature -> feature.getType() == TileSectionType.ROAD
+                        && !feature.isComplete(),
+                "road"));
+
+        placeTileAndUpdate(tile11, 0, 1);
+
+        features = this.featureManager.getFeatures();
+        assertThat(features).hasSize(2);
+        assertThat(features).areExactly(1, new Condition<>(
+                feature -> feature.getType() == TileSectionType.CITY
+                        && !feature.isComplete(),
+                "city"));
+        assertThat(features).areExactly(1, new Condition<>(
+                feature -> feature.getType() == TileSectionType.ROAD
+                        && !feature.isComplete(),
+                "road"));
+
+        placeTileAndUpdate(tile20, 1, 1);
+
+        features = this.featureManager.getFeatures();
+        assertThat(features).hasSize(2);
+        assertThat(features).areExactly(1, new Condition<>(
+                feature -> feature.getType() == TileSectionType.CITY
+                        && feature.isComplete(),
+                "city"));
+        assertThat(features).areExactly(1, new Condition<>(
+                feature -> feature.getType() == TileSectionType.ROAD
+                        && !feature.isComplete(),
+                "road"));
+    }
+
+    @Test
+    public void shouldUpdateFeatures_circleRoad() {
+        Tile tile15 = TileStackFactory.getTile(15);
+        Tile tile2 = TileStackFactory.getTile(2);
+        Tile tile10 = TileStackFactory.getTile(10);
+        tile10.rotateClockwise();
+        Tile tile7 = TileStackFactory.getTile(7);
+        tile7.rotateClockwise();
+        tile7.rotateClockwise();
+        Tile tile14 = TileStackFactory.getTile(14);
+
+        placeTileAndUpdate(tile15, 1, 0);
+        placeTileAndUpdate(tile2, 1, -1);
+        placeTileAndUpdate(tile10, 0, -1);
+        placeTileAndUpdate(tile7, -1, -1);
+        placeTileAndUpdate(tile14, -1, 0);
+
+        Collection<? extends Feature> features = this.featureManager.getFeatures();
+        assertThat(features).hasSize(7);
+
+        assertThat(features).areExactly(2, new Condition<>(
+                feature -> feature.getType() == TileSectionType.ROAD
+                && feature.isComplete(),
+                "road"));
+        assertThat(features).areExactly(3, new Condition<>(
+                feature -> feature.getType() == TileSectionType.ROAD
+                        && !feature.isComplete(),
+                "road"));
+        assertThat(features).areExactly(2, new Condition<>(
+                feature -> feature.getType() == TileSectionType.CITY
+                && !feature.isComplete(),
+                "city"));
+    }
+
+    private void placeTileAndUpdate(Tile tile, int xPosition, int yPosition) {
+        assertThat(PlacementValidator.isValid(this.board, xPosition, yPosition, tile)).isTrue();
+        this.board.placeTile(tile, xPosition, yPosition);
+        this.featureManager.updateFeatures(tile, xPosition, yPosition);
     }
 }
